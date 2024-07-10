8b120cb307fff175a8b73b75622497bd8a23283c
yukebrillianth <yukebrilliant@gmail.com>
2024-07-10 19:22:38 +0700

    feat: initial project

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Type check TypeScript files
+  '**/*.(ts|tsx)': () => 'pnpm typecheck',
+
+  // Lint & Prettify TS and JS files
+  '**/*.(ts|tsx|js)': () => [`pnpm lint:strict `, `pnpm format:write`],
+
+  // Prettify only Markdown and JSON files
+  '**/*.(md|json)': () => `pnpm format:write`,
+};
