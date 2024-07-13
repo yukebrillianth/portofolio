8818e4708229eeea8e74b3d267bf9216451c64ea
yukebrillianth <yukebrilliant@gmail.com>
2024-07-14 03:00:09 +0700

    feat: api wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import Image from 'next/image';
+
+import { PortfolioService } from '@/services';
+
+export default async function Home() {
+  const data = await PortfolioService.getPortfolios();
   return (
     <main>
-      <p>ok</p>
+      <h1>Posts</h1>
+      {data.portfolios.map((portfolio) => (
+        <>
+          <p key={portfolio.id}>{portfolio.title}</p>
+          <Image
+            alt='s'
+            src={portfolio.images[0].url}
+            width={400}
+            height={200}
+            className='rounded-lg'
+          />
+        </>
+      ))}
     </main>
   );
 }
---
+++ src/services/index.ts
@@ -0,0 +1,1 @@
+export * as PortfolioService from './portfolio.service';
